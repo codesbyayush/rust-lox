1e2164377ed6eee38b4d2c39a849e7ab433fe4d9
Ayush <codesbyayush.gmail.com>
2024-08-26 11:40:25 +0530

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -22,14 +22,14 @@ fn main() {
             });
 
             // Uncomment this block to pass the first stage
-            if !file_contents.is_empty() {
-                panic!("Scanner not implemented");
-            } else {
+            // if !file_contents.is_empty() {
+            //     panic!("Scanner not implemented");
+            // } else {
             //     println!("EOF  null"); // Placeholder, remove this line when implementing the scanner
             // }
 
             tokenize(&file_contents);
-            }
+            // }
         }
         _ => {
             eprintln!( "Unknown command: {}", command);
