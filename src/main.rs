a68576d4e2eb6df0428b88ef4a067483bee34aa1
Ayush <codesbyayush.gmail.com>
2024-08-26 18:43:44 +0530

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -126,7 +126,7 @@ fn tokenize(file_contents: &str) {
     }
 
     if ongoing_string {
-        println!("[line {}] Error: Unterminated string.", curr_line);
+        eprintln!("[line {}] Error: Unterminated string.", curr_line);
         exit_code = 65;
     }
     println!("EOF  null");
