0fa0dd43aa09fcde30adf271a8ceb95564605a03
Ayush <codesbyayush.gmail.com>
2024-08-26 18:41:02 +0530

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -14,23 +14,14 @@ fn main() {
 
     match command.as_str() {
         "tokenize" => {
-            // You can use print statements as follows for debugging, they'll be visible when running tests.
+
             eprintln!("Logs from your program will appear here!");
 
             let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
                 eprintln!( "Failed to read file {}", filename);
                 String::new()
             });
-
-            // Uncomment this block to pass the first stage
-            // if !file_contents.is_empty() {
-            //     panic!("Scanner not implemented");
-            // } else {
-            //     println!("EOF  null"); // Placeholder, remove this line when implementing the scanner
-            // }
-
             tokenize(&file_contents);
-            // }
         }
         _ => {
             eprintln!( "Unknown command: {}", command);
@@ -40,39 +31,54 @@ fn main() {
 }
 
 fn tokenize(file_contents: &str) {
-    let mut code = 0;
+    let mut exit_code = 0;
     let characters = file_contents.chars();
     let mut curr_line = 1;
-    let mut last = ' ';
-    let mut comment = false;
-    for (_, c) in characters.enumerate() {
-        if comment && c == '\n' {
-            comment = false;
-            curr_line += 1;
-            continue;
-        } else if comment {
+    let mut last_word = ' ';
+    let mut is_comment = false;
+    let mut ongoing_string = false;
+    let mut string_start = 0;
+    for (i, c) in characters.enumerate() {
+        if ongoing_string {
+            if c == '\n' {
+                curr_line += 1;
+            } else if c == '"' {
+                let string_literal = &file_contents[string_start..(i)];
+                println!("STRING {}\" {}", &string_literal, &string_literal[1..]);
+                ongoing_string = false;
+            }
             continue;
         }
-        if last == '/' && c == '/' {
-            last = ' '; 
-            comment = true;
+        if c == '"' {
+            ongoing_string = true;
+            string_start = i;
             continue;
-        } else if last == '/' {
+        }
+        if is_comment {
+            if c == '\n' {
+                is_comment = false;
+                curr_line += 1;
+            }
+            continue;
+        }
+
+        if last_word == '/' {
+            last_word = ' ';
+            if c == '/' {
+                is_comment = true;
+                continue;
+            }
             println!("SLASH / null");
-            
-            last = ' ';
         }
-        if c != '=' && last != ' ' {
-            let mut found = true;
-            match last {
+        if c != '=' && last_word != ' ' {
+            match last_word {
                 '=' => println!("EQUAL = null"),
                 '!' => println!("BANG ! null"),
                 '<' => println!("LESS < null"),
                 '>' => println!("GREATER > null"),
-                _ => found = false
+                _ => {}
             }
-            if !found { last = '=';}
-            else { last = ' ';}
+            last_word = ' ';
         } 
         
         match c {
@@ -88,29 +94,29 @@ fn tokenize(file_contents: &str) {
             ';' =>    println!("SEMICOLON ; null"), 
             '=' =>    {
                 let mut found = true;
-                match last {
+                match last_word {
                     '=' => println!("EQUAL_EQUAL == null"),
                     '!' => println!("BANG_EQUAL != null"),
                     '<' => println!("LESS_EQUAL <= null"),
                     '>' => println!("GREATER_EQUAL >= null"),
                     _ => found = false
                 }
-                if !found { last = '=';}
-                else { last = ' ';}
+                if !found { last_word = '=';}
+                else { last_word = ' ';}
             },
-            '!' =>    last = '!',
-            '<' =>    last = '<',
-            '>' =>    last = '>',
-            '/' =>    last = '/',
+            '!' =>    last_word = '!',
+            '<' =>    last_word = '<',
+            '>' =>    last_word = '>',
+            '/' =>    last_word = '/',
             '\n' =>   curr_line += 1,
             '\t' | '\r' | ' ' =>   {},
             u => {
                 eprintln!("[line {}] Error: Unexpected character: {}", curr_line, u);
-                code = 65;
+                exit_code = 65;
             }
         };
     }
-    match last {
+    match last_word {
         '=' => println!("EQUAL = null"),
         '!' => println!("BANG ! null"),
         '<' => println!("LESS < null"),
@@ -118,6 +124,10 @@ fn tokenize(file_contents: &str) {
         '/' => println!("SLASH / null"),
         _ => {}
     }
+
+    if ongoing_string {
+        println!("[line {}] Error: Unterminated string.", curr_line);
+    }
     println!("EOF  null");
-    exit(code);
+    exit(exit_code);
 }
